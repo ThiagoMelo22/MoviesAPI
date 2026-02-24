36666882ec79fbce5aebf828d5ab252c6ecd9c1e
ThiagoMelo22 <thiagomelo329@hotmail.com>
2026-02-24 17:10:58 -0300

    Adding Add Movie method

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost("AddMovie")]
+    public void AddMovie([FromBody] Movie movie) 
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Duration);
+    }
+}
